bf85beb4aff1f81e6281fec6903f253b80cae8c7
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-08-06 10:28:12 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+
+
+console.log ("Hola")
+
+var Receta: = (0)
+
+console.log (receta);
